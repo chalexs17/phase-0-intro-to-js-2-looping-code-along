b22db1dd2da5030f8f9e8d54bd1f370d18e69692
Alexis Chavez <alexis.chavez17@outlook.com>
2023-03-11 19:02:46 -0700

    Complete Looping code along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages
+}
+
+const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result);
+
+function countDown(number){
+    while (number >=0){
+        console.log(number);
+        number--;
+    }
+}
